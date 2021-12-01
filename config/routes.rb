3ee6ab74f2d8df6dc9b06cd33da8ea5e0f5a1efe
Mauricio Mora <mauricios.mora@gmail.com>
2021-12-01 20:03:35 -0300

    modificacion rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,22 @@ Rails.application.routes.draw do
   get 'home/indexVisit'
   get 'home/indexUser'
 
-  devise_for :veterinaries
-  devise_for :admins
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :veterinaries, controllers: {
+    sessions: 'veterinaries/sessions',
+    passwords: 'veterinaries/passwords',
+    registrations: 'veterinaries/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#indexVisit"
 end
