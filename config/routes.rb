29823c94212ebf42adb0d0f0a85ba5d8f7146cea
Mauricio Mora <mauricios.mora@gmail.com>
2021-12-01 19:54:16 -0300

    Se agrega controlador home con vista index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/indexVisit'
+  get 'home/indexUser'
+
   devise_for :veterinaries
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "home#indexVisit"
 end
