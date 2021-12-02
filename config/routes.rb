e51cefd53cafbe55f1ab20cf722079c26ca55c7a
Mauricio Mora <mauricios.mora@gmail.com>
2021-12-02 15:45:29 -0300

    Comienza modelo post

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   get 'home/indexVisit'
   get 'home/indexUser'
-
+  
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
